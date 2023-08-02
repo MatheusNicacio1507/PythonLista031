a5a803b88c49d8504b210e7463f85c339fc4e795
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-02 10:29:42 -0300

    Commit da questão 01

---
+++ questao01.py
@@ -0,0 +1,12 @@
+'''
+Desenvolver um programa que pergunte ao usuário o seu nome completo e seu sexo. Em seguida, o programa
+deve apresentar os dados anteriormente informados.
+'''
+
+print("Qual o seu nome completo e seu sexo?")
+
+nome = (input("Insira o seu nome:"))
+
+sexo = (input("Insira seu sexo:"))
+
+print("Seu nome é", nome,"e seu sexo é", sexo)
