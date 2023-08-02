37759d64b12e05d85b3a901789d667279a506674
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-02 10:44:42 -0300

    Commit da questão 03

---
+++ questao03.py
@@ -0,0 +1,12 @@
+'''
+Fazer um programa que pergunte um valor em Dólares e apresente o equivalente em Reais. Considere U$1,00 =
+R$3,80.
+'''
+
+print("Insira um valor em Dólares $:")
+
+doll = float(input("Inserir valor: $"))
+
+doll = doll * 3.80
+
+print("O valor em Reais é: R$", doll)
