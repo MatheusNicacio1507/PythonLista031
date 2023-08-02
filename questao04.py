d8a7ca90ec76f1ea4bb7d0635a848f3848bb7f92
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-02 10:47:51 -0300

    Commit da questão 04

---
+++ questao04.py
@@ -0,0 +1,10 @@
+'''
+Fazer um programa que pergunte um número inteiro e apresente o seu antecessor e seu sucessor.
+'''
+
+numero = int(input("Informe um número inteiro:"))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("O antecessor é:", antecessor, "e o sucessor é:", sucessor)
