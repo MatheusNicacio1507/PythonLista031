578f167645f26f96e75c86c944af0396a7615d23
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-02 11:12:42 -0300

    Commit da questão 07

---
+++ questao07.py
@@ -0,0 +1,13 @@
+'''
+Fazer um algoritmo que pergunte 3 números e apresente a média aritmética entre estes 3 números.
+'''
+
+print("Abaixo, escreva três números")
+
+n1 = float(input("Informe o primeiro número:"))
+n2 = float(input("Informe o segundo número:"))
+n3 = float(input("Informe o terceiro número"))
+
+media = (n1 + n2 + n3) / 3
+
+print("A média entre esses números é", media)
