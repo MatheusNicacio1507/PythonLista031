24294441dd1480860f8afdd6015aa27c6bd56804
matheus Nicacio <matheussantosnicacio@gmail.com>
2023-08-14 09:21:48 -0300

    formatação questão 2

--- lista031/questao02.py
+++ lista031/questao02.py
@@ -6,13 +6,13 @@ b) Resultado de suas multiplicações
 
 print("Abaixo, escreva quatro valores inteiros")
 
-valor1 = int(input("Insira o primeiro valor inteiro:"))
-valor2 = int(input("Insira o segundo valor inteiro:"))
-valor3 = int(input("Insira o terceiro valor inteiro:"))
-valor4 = int(input("Insira o quarto valor inteiro:"))
+valor1 = int(input("Insira o primeiro valor inteiro: "))
+valor2 = int(input("Insira o segundo valor inteiro: "))
+valor3 = int(input("Insira o terceiro valor inteiro: "))
+valor4 = int(input("Insira o quarto valor inteiro: "))
 
 soma = valor1 + valor2 + valor3 + valor4
 
 multi = valor1 * valor2 * valor3 * valor4
 
-print("A adição desses números é =", soma, "e a multiplicação entre esses números é =", multi)
+print(f"A adição desses números é = {soma} e a multiplicação entre 1esses números é = {multi}")
